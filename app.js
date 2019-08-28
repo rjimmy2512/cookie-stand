f70d993d9d7f15f90e528642692bf6f55a149bd2
R.James <rjimmy2512@gmail.com>
2019-08-27 22:36:28 -0700

    js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 var tableEl = document.getElementById('table');
 var allLocations = [];
 // constructor for locations:
-function Location(name, '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', 'Daily Location Total'){
-  this.name = name;
+function Location('name', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', 'Daily Location Total'){
+  this.name = 'name';
   this.cookiesAt6am = '6:00am';
   this.cookiesAt7am = '7:00am';
   this.cookiesAt8am = '8:00am';
