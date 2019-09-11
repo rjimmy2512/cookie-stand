d10f0d6d9b92f097520cb671fd7ff96ea0b61ffd
R.James <rjimmy2512@gmail.com>
2019-09-10 22:31:26 -0700

    master pr

--- app.js
+++ app.js
@@ -1,187 +1,134 @@
 'use strict';
 
-var tableEl = document.getElementById('table-sales');
-var tbodyEl = document.createElement('tbody');
-
-var allShops = [];
-var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-function Shop(name, minCustomer, maxCustomer, avgCookiesPerSale) {
+// gets the form DOM element
+var addLocationForm = document.getElementById('newLocation');
+// gets the table element
+var table = document.getElementById('table-sales');
+//holds the instances of the objects entered in the constructor function
+var storesAll = [];
+var timeArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+//constructor function
+function Locations (name, min, max, averageCookieSale) {
   this.name = name;
-  this.minCustomer = minCustomer;
-  this.maxCustomer = maxCustomer;
-  this.avgCookiesPerSale = avgCookiesPerSale;
-  this.cookiesEachHourArr = [];
+  this.minCustomers = min;
+  this.maxCustomers = max;
+  this.averageCookieSale = averageCookieSale;
+  this.randomCustomerArray = [];
+  this.cookiesPerHour = [];
+  this.totalCookies = 0;
 
-  allShops.push(this);
+  storesAll.push(this);
 }
-
-console.log(this);
-
-Shop.prototype.calcCookiesSoldEachHour = function(minCustomer, maxCustomer, avgCookies){
-  for(var i = 0; i < hoursOpen.length; i++){
-    //for each hour open, get random number of customers
-    var customersEachHour = Math.random() * (maxCustomer - minCustomer) + minCustomer;
-    //calculate cookies each hour and append to array
-    var cookiesEachHour = Math.floor(customersEachHour * avgCookies);
-    this.cookiesEachHourArr.push(cookiesEachHour);
+//prototypes
+// generates our randomCustomerArray
+Locations.prototype.calcCustomersPerHour = function() {
+  for(var i = 0; i < timeArray.length; i++) {  
+    var randomNumberOfCustomers = randomNumber(
+      this.minCustomers,
+      this.maxCustomers
+    );
+    this.randomCustomerArray.push(randomNumberOfCustomers);
   }
 };
-
-Shop.prototype.render = function() {
-  this.calcCookiesSoldEachHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerSale);
-
-  //create a row and append to tbody
-  var trEl = document.createElement('tr');
-  tbodyEl.appendChild(trEl);
-
-  //create and fill first td with shop name
-  var tdEl = document.createElement('td');
-  tdEl.textContent = this.name;
-  trEl.appendChild(tdEl);
-  console.log('first column shop name ' + this.name);
-
-  var cookiesTotal = 0;
-  //for each item in the shop's cookiesEachHourArr
-  for(var h = 0; h < this.cookiesEachHourArr.length; h++) {
-    cookiesTotal += this.cookiesEachHourArr[h];
-    //create and fill td
-    tdEl = document.createElement('td');
-    tdEl.textContent = this.cookiesEachHourArr[h];
-    //append to row
-    trEl.appendChild(tdEl);
+// generating our cookiesPerHour array
+// calculating our total cookies for the day for the location
+Locations.prototype.calcCookiesPerHour = function() {
+  for (var i=0; i < timeArray.length; i++) {
+    var cookiesSold = Math.floor(this.randomCustomerArray[i] * this.averageCookieSale);
+    this.cookiesPerHour.push(cookiesSold);
+    this.totalCookies = this.totalCookies + cookiesSold;
   }
-
-  tdEl = document.createElement('td');
-  tdEl.textContent = cookiesTotal;
-  trEl.appendChild(tdEl);
 };
-
-new Shop('1st and Pike', 23, 65, 6.3);
-new Shop('SeaTac Airport', 3, 24, 1.2);
-new Shop('Seattle Center', 11, 38, 3.7);
-new Shop('Capitol Hill', 20, 38, 2.3);
-new Shop('Alki', 2, 16,4.6);
-
-//render header
-function renderHeader() {
-  //create header section
-  var theadEl = document.createElement('thead');  tableEl.appendChild(theadEl);
-  //create header row
-  var trEl = document.createElement('tr');
-  theadEl.appendChild(trEl);
-  //create blank td
-  var tdEl = document.createElement('td');
-  trEl.appendChild(tdEl);
-  //for each hour open, create and fill td
-  for(var i = 0; i < hoursOpen.length; i++) {
-    tdEl = document.createElement('td');
-    tdEl.textContent = hoursOpen[i];
-    trEl.appendChild(tdEl);
+// call the methods that generate our arrays.
+Locations.prototype.render = function() {
+  this.calcCustomersPerHour();
+  this.calcCookiesPerHour();
+   // create a tr
+  var trEl = addElement('tr', false, table);
+  // need the name
+  addElement('td', this.name, trEl);
+  // need cookies sold each hour
+  for(var i = 0; i < timeArray.length; i++){
+    addElement('td', this.cookiesPerHour[i], trEl);
+  }
+  // need total for location
+  addElement('td', this.totalCookies, trEl);
+};
+//object instances
+new Locations ('1st and Pike', 23, 65, 6.3);
+new Locations ('SeaTac Airport', 3, 24, 1.2);
+new Locations ('Seattle Center', 11, 38, 3.7);
+new Locations ('Capitol Hill', 20, 38, 2.3);
+new Locations ('Alki', 2, 16, 4.6);
+//functions
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//top row
+function renderHeader(){
+   // need a table row
+  var trEl = addElement('tr', false, table);
+  // need an empty cell
+  addElement('td', '', trEl);
+  // need a loop over the time
+  for(var i = 0; i < timeArray.length; i++){
+    addElement('td', timeArray[i], trEl);
   }
   //create Daily Location Total
-  tdEl = document.createElement('td');
-  tdEl.textContent = 'Daily Location Total';
-  trEl.appendChild(tdEl);
-
-  //create the table body section
-  tableEl.appendChild(tbodyEl);
-
-  //for each shop in the allShops array, call the shop's render method
-  for(var s =  0; s < allShops.length; s++) {
-    allShops[s].render();
-  }
+  var tdEl = addElement('td','Daily Location Total', trEl);
 }
 
-//render footer
-function renderFooter() {
-  //create footer section
-  var tfootEl = document.createElement('tfoot');
-  tableEl.appendChild(tfootEl);
-  //create footer row
-  var trEl = document.createElement('tr');
-  tfootEl.appendChild(trEl);
-  //create Totals td
-  var tdEl = document.createElement('td');
-  tdEl.textContent = 'Totals';
-  trEl.appendChild(tdEl);
+function renderTable (){
+  //clears all values from the table
+  document.getElementsByTagName('tbody')[0].innerHTML = '';
+  renderHeader();
+  //renders each location
+  for (var i = 0; i < storesAll.length; i++) {
+    storesAll[i].render();
+  }
+  renderFooter ();
+}
 
-  var grandTotal = 0;
-  //for each hour open
-  for(var i = 0; i < hoursOpen.length; i++) {
-    //variable to hold running total for the current hour
-    var hourTotal = 0;
-    //loop through each property's cookiesEachHourArr and sum up the column of data
-    for(var t =  0; t < allShops.length; t++) {
-      hourTotal += allShops[t].cookiesEachHourArr[i];
+function renderFooter (){
+  // make and add a row to the table
+  var trEl = addElement('tr', false, table);
+  // create the first cell with the text content: 'Total Each Hour'
+  addElement('td', 'Total Each Hour', trEl);
+  var totalOfAllTotals = 0;
+  // loop through the array of the hours
+  for(var i = 0; i < timeArray.length; i++){
+    var hourlyTotal = 0;
+    for(var j = 0; j < storesAll.length; j++){
+      hourlyTotal += storesAll[j].cookiesPerHour[i];
     }
-    tdEl = document.createElement('td');
-    tdEl.textContent = hourTotal;
-    trEl.appendChild(tdEl);
-    grandTotal += hourTotal;
+    totalOfAllTotals += hourlyTotal;
+    addElement('td', hourlyTotal, trEl);
   }
-  //create td for grand total
-  tdEl = document.createElement('td');
-  tdEl.textContent = grandTotal;
-  trEl.appendChild(tdEl);
+  addElement('td', totalOfAllTotals, trEl);
 }
+// MAGIC FUNCTION////////////////////////////////////
+// when I call this, my element needs to be a string
+function addElement(element, content, parent){
+  var newElement = document.createElement(element);
+  if(content){
+    var newContent = document.createTextNode(content);
+    newElement.appendChild(newContent);
+  }
+  parent.appendChild(newElement);
+  return newElement;
+}
+//Function for adding new sale location
+function addLocation (event){
+  event.preventDefault();
 
-renderHeader();
-renderFooter();
+  var name = event.target.name.value;
+  var min = event.target.min.value;
+  var max = event.target.max.value;
+  var averageCookieSale = event.target.averageCookieSale.value;
 
-//add shop function
-function addRow() {
-// get input values
-var name = document.getElementById('name').value;
-var minCustomers = document.getElementById('mincustomers').value;
-var maxCustomers = document.getElementById('maxCustomers').value;
-var avgCookiesPerSale = document.getElementById('avgCookiesPerSale').value;
-
-  // get the html table
-// 6 = the first table
-var table = document.getElementsByTagName('table-sales')[6];                 
-
-  // add new empty row to the table
-// 0 = in the top 
-// table.rows.length = the end
-// table.rows.length/2+1 = the center
-var newRow = table.insertRow(table.rows.length/2+1);
-// add cells to the row
-var cel1 = newRow.insertCell(0);
-var cel2 = newRow.insertCell(1);
-var cel3 = newRow.insertCell(2);
-var cel4 = newRow.insertCell(3);
-var cel5 = newRow.insertCell(4);
-var cel6 = newRow.insertCell(5);
-var cel7 = newRow.insertCell(6);
-var cel8 = newRow.insertCell(7);
-var cel9 = newRow.insertCell(8);
-var cel10 = newRow.insertCell(9);
-var cel11 = newRow.insertCell(10);
-var cel12 = newRow.insertCell(11);
-var cel13 = newRow.insertCell(12);
-var cel14 = newRow.insertCell(13);
-var cel15 = newRow.insertCell(14);
-var cel16 = newRow.insertCell(15);
-var cel17 = newRow.insertCell(16);
-                 
-// add values to the cells
-//cel1.innerHTML = name;
-//cel2.innerHTML = age;
-//cel3.innerHTML = age;
-//cel4.innerHTML = name;
-//cel5.innerHTML = lname;
-//cel6.innerHTML = age;
-//cel7.innerHTML = name;
-//cel8.innerHTML = lname;
-//cel9.innerHTML = age;
-//cel10.innerHTML = name;
-//cel11.innerHTML = lname;
-//cel12.innerHTML = age;
-//cel13.innerHTML = name;
-//cel14.innerHTML = lname;
-//cel15.innerHTML = age;
-//cel16.innerHTML = name;
-//cel17.innerHTML = age;
-};
-
+  new Locations(name, min, max, averageCookieSale);
+  renderTable();
+}
+// event listener
+addLocationForm.addEventListener('submit', addLocation);
+renderTable();
